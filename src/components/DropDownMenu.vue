a7d3f1519d459605ee8056f8e8b19b2656679037
webferrol <xurxo@webferrol.com>
2022-09-07 00:35:43 +0200

    Creada la página WebFerrol con typescript
    -Template público
    -Toolbar del menú y pie de página

---
+++ src/components/DropDownMenu.vue
@@ -0,0 +1,50 @@
+<template>
+  <q-btn-dropdown stretch flat :icon="icon" v-model="menuState">
+    <div class="dropdown-list">
+      <q-list>
+        <q-item-label header>Acceso</q-item-label>
+        <q-item clickable v-close-popup tabindex="0">
+          <q-item-section avatar>
+            <q-avatar icon="login" color="secondary" text-color="white" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Login/Sign in</q-item-label>
+            <q-item-label caption>Acceso Backend</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-separator inset spaced />
+        <q-item-label header>Files</q-item-label>
+        <q-item v-for="n in 3" :key="`y.${n}`" clickable v-close-popup tabindex="0">
+          <q-item-section avatar>
+            <q-avatar icon="assignment" color="primary" text-color="white" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Vacation</q-item-label>
+            <q-item-label caption>February 22, 2016</q-item-label>
+          </q-item-section>
+          <q-item-section side>
+            <q-icon name="info" />
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </div>
+  </q-btn-dropdown>
+</template>
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import type { Ref } from 'vue';
+
+
+const menuState: Ref<boolean> = ref(false);//Detectamos si el menú está abierto o cerrado
+
+const icon = computed((): string => !menuState.value ? 'menu' : 'menu_open');//Según el estado del menú (abierto/cerrado) insertamos un icono u otro
+
+
+
+</script>
+
+<style scoped>
+.dropdown-list {
+  max-width: 350px;
+}
+</style>
---
+++ src/layouts/MainLayoutPublic.vue
@@ -0,0 +1,48 @@
+<template>
+    <q-layout view="hHh lpR fFf">
+
+        <q-header reveal bordered class="bg-primary text-white">
+
+            <q-toolbar class="shadow-2 rounded-borders">
+                <q-toolbar-title>WebFerrol</q-toolbar-title>
+                <drop-down-menu></drop-down-menu>
+            </q-toolbar>
+
+            <!-- <q-toolbar>
+                <q-toolbar-title>
+                    <HorizontalMenu></HorizontalMenu>
+                </q-toolbar-title>
+            </q-toolbar> -->
+        </q-header>
+
+        <q-page-container>
+            <router-view />
+        </q-page-container>
+
+        <q-footer elevated class="bg-grey-8 text-white">
+            <q-toolbar class="shadow-2 rounded-borders">
+                <q-toolbar-title>WebFerrol</q-toolbar-title>
+                
+
+                <!--
+        notice shrink property since we are placing it
+        as child of QToolbar
+      -->
+                <q-tabs v-model="tab"  shrink>
+                    <q-tab name="tab1" label="Tab 1" />
+                    <q-tab name="tab2" label="Tab 2" />
+                    <q-tab name="tab3" label="Tab 3" />
+                </q-tabs>
+            </q-toolbar>
+        </q-footer>
+
+    </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import type { Ref } from 'vue';
+import DropDownMenu from 'src/components/DropDownMenu.vue';
+
+const tab: Ref<string> = ref('');
+</script>

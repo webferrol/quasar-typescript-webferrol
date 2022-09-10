5821a0a3ca5f9a66bef2fb7d7039ca620a011c17
webferrol <xurxo@webferrol.com>
2022-09-10 14:25:10 +0200

    Mejorado el código de acceso a usuario. Las promesas se encuentran en el store/users.ts quedando un formulario más limpio para el registro/acceso de usuario

--- src/pages/SignIn.vue
+++ src/pages/SignIn.vue
@@ -6,7 +6,7 @@
                 <h2 class="text-h4 text-center text-primary">Sign In</h2>
                 <div class="q-pa-md">
 
-                    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+                    <q-form @submit="signIn({email, password, accept})" @reset="onReset" class="q-gutter-md">
                         <q-input filled v-model.trim="email" label="Su email *" hint="Correo electrónico" lazy-rules
                             :rules="[isValidEmail]" />
 
@@ -21,10 +21,10 @@
                             <q-btn label="Borrar" type="reset" color="primary" flat class="q-ml-sm" />
                         </div>
                     </q-form>
-                    <q-btn class="q-mt-sm" @click="handleSendPasswordResetEmail" :loading="loading" flat
+                    <q-btn class="q-mt-sm" @click="onSendPasswordResetEmail(email)" :loading="loading" flat
                         style="color: #FF0080" label="¿Olvidaste la contraseña?" />
-                    <q-btn class="q-mt-sm" :to="{ name: 'Register' }" flat style="color: #FF0080"
-                        label="Regístrate como nuevo usuario" />
+                    <!-- <q-btn class="q-mt-sm" :to="{ name: 'Register' }" flat style="color: #FF0080"
+                        label="Regístrate como nuevo usuario" /> -->
                 </div>
             </div>
         </div>
@@ -34,34 +34,30 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { useStoreUsers } from 'src/stores/users';
-import { useNotify } from 'src/composables/notify.hook';
 import { storeToRefs } from 'pinia';
-const { error: $error } = useNotify();
-const {signIn} = useStoreUsers();
-const { loading, error } = storeToRefs(useStoreUsers());
+
+const {signIn,onSendPasswordResetEmail} = useStoreUsers();
+const { loading } = storeToRefs(useStoreUsers());
 const email = ref<string>('');
 const password = ref<string>('');
 const accept = ref<boolean>(false);
 
-//Botón de envío
-const onSubmit = async () => {
-    if (accept.value !== true)
-            $error('Primero debe aceptar la licencia y los términos');
-    else{
-        await signIn({ email: email.value, password: password.value });
-        if(error.value)
-            $error(error.value);
-    }
+
+
+const onReset = () => {
+    email.value = '';
+    password.value = '';
+    accept.value = false;
+};
+
+
+/**
+ * Función que valida un correo a través de una expresión regular
+ * @param {string} email - Dirección de correo electrónico
+ * @returns {boolean|string} - Verdadero si pasa el test o String(Mensaje si no lo pasa)
+ */
+const isValidEmail = ( email: string ) => {
+    const emailPattern = /^(?=[a-zA-Z0-9@._%+-]{6,254}$)[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/;
+    return emailPattern.test(email) || `La dirección de correo "${email}" no es válida`;
 }
-    const onReset = () => {};
-    const handleSendPasswordResetEmail = () => {}
-    /**
-     * Función que valida un correo a través de una expresión regular
-     * @param {String} email - Supuestamente un dirección de correo electrónico
-     * @returns {Bool|String} - Verdadero si pasa el test o String(Mensaje si no lo pasa)
-     */
-    const isValidEmail = (email: string) => {
-        const emailPattern = /^(?=[a-zA-Z0-9@._%+-]{6,254}$)[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/;
-        return emailPattern.test(email) || `La dirección de correo "${email}" no es válida`;
-    }
 </script>
--- src/stores/users.ts
+++ src/stores/users.ts
@@ -1,19 +1,23 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
+import { useRouter } from "vue-router";
 import { auth } from "src/firebase";
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, updateProfile, updateEmail, sendPasswordResetEmail, updatePassword, signOut } from "firebase/auth";
+
+import { useNotify } from "src/composables/notify.hook";
 
 
 export const useStoreUsers = defineStore('users', () => {
     const user = ref<object | null>(null);
-    const error = ref<any>(false);
     const loading = ref<boolean>(false);
+    const { error,ok } = useNotify();
+    const router = useRouter();
 
      //Mensajes de error con la técnica "Dynamic Property Maps"
   const errorMessages:any = {
     'auth/too-many-requests': 'Demasiados intentos vuelva a intentarlo más tarde',
-    'auth/user-not-found':'Usuario o contraseña no válida',
-    'auth/invalid-password':'Usuario o contraseña no válida',
+    'auth/user-not-found':'Usuario NO registrado o contraseña no válida',
+    'auth/invalid-password':'Usuario NO registrado o contraseña no válida',
 }
 
     /**
@@ -27,26 +31,47 @@ export const useStoreUsers = defineStore('users', () => {
     /**
      * Método que nos permite autentificar un usuario en la api 
     Autenticación de Firebase
-    * @param {Object}  - Se trata de un objeto desestructurado donde se nos pasa el usuario y la contraseña
+    * @param { object }  - Se trata de un objeto desestructurado donde se nos pasa el usuario y la contraseña
     * @link https://firebase.google.com/docs/auth/web/password-auth?hl=es&authuser=0
     */
-    const signIn = async ({ email, password }:Record<string,string>) => {
+    const signIn = async ({ email, password, accept }:{email: string, password: string, accept: boolean}) => {        
         loading.value = true;
         try {
+            if (accept !== true)
+                throw new Error('Primero debe aceptar la licencia y los términos');
             const userCredential = await signInWithEmailAndPassword(auth, email, password);
             setUser(userCredential.user);
+            router.push({name: 'WorkExperience'});
         } catch (p_error:any) {
-           error.value = errorMessages[p_error.code] || p_error.message;
+            error(errorMessages[p_error.code] || p_error.message);          
            console.log('Código',p_error.code)
         }  finally{
            loading.value = false;
         }      
     }
 
+    /**
+     * Función para enviar un correo electrónico de restablecimiento de contraseña a un usuario
+     * @param {String} email 
+     */
+     const onSendPasswordResetEmail = async ( email :string) => {
+        
+        try { 
+            loading.value = true;
+            await sendPasswordResetEmail(auth, email);
+            ok(`Se ha enviado a su cuenta de correo ${email} las instrucciones para crear una nueva contraseña. Si no lo encuentra compruebe su bandeja de SPAN`);
+        } catch (p_error:any) {
+            error(errorMessages[p_error.code] || p_error.message);
+            console.log('Código',p_error.code)
+        } finally {
+            loading.value = false;
+        }        
+    }
+
     return {
         user,
-        error,
         loading,
         signIn,
+        onSendPasswordResetEmail,
     }
 });
